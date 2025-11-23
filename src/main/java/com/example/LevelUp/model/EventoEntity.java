c419105b09b4465eb1203c6f4a044d4795a5614d
Daniela <dani.paz.poblete@gmail.com>
2025-11-23 00:14:51 -0300

    eliminacion de comentarios entity

--- src/main/java/com/example/LevelUp/model/EventoEntity.java
+++ src/main/java/com/example/LevelUp/model/EventoEntity.java
@@ -6,7 +6,7 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 
 @Entity
-@Data //lombok que hace todos los getter y setter
+@Data
 @Table(name = "evento")
 @AllArgsConstructor
 @NoArgsConstructor
